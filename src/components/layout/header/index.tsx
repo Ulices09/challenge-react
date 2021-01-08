ea3a5120f5c68b3ff5a16a2c0e850716455fc372
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-01-07 21:58:28 -0500

    Add cart icon

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from 'react';
 import styles from './header.module.css';
 import logo from '../../../assets/logo.svg';
+import cart from '../../../assets/cart.svg';
+import { useHistory } from 'react-router-dom';
 
 const Header: FC = () => {
+    const history = useHistory();
+
     return (
         <div className={styles.header}>
-            <div className={styles.sideContent}>
+            <div
+                className={styles.sideContent}
+                onClick={() => history.push('/')}
+            >
                 <img src={logo} className={styles.logo} alt="logo" />
-                <h2>My Shop</h2>
+                <h2 className={styles.title}>My Shop</h2>
             </div>
             <div className={styles.sideContent}>
-                <h2>Carrex</h2>
+                <img src={cart} className={styles.cart} alt="cart" />
+                <span className="ml-2">1</span>
             </div>
         </div>
     );
